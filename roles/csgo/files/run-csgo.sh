208ed89057074768e328c1753c48314d693ce56a
Jacob Jenner Rasmussen <jacob.mindstorms@gmail.com>
2019-01-25 16:40:09 +0100

    pre-event changes

--- roles/csgo/files/run-csgo.sh
+++ roles/csgo/files/run-csgo.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
-/home/steam/csgo/srcds_linux \
+export LD_LIBRARY_PATH=/home/steam/csgo/bin:/home/steam/csgo:/home/steam/csgo/bin
+exec /home/steam/csgo/srcds_linux \
+  -game csgo \
   -net_port_try 1 \
-  -usercon \
+  -tickrate 128 \
+  -ip 0.0.0.0 \
   -port $SERVER_PORT \
+  -usercon \
   +sv_setsteamaccount "$GSLT" \
+  +tv_port "$GOTV_PORT" \
+  +tv_password "$GOTV_PASSWORD" \
+  +tv_relaypassword "$GOTV_PASSWORD_ggwrelay" \
+  +sv_password "$PASSWORD" \
+  +rcon_password "$RCON_PASSWORD" \
   +mapgroup mg_active \
+  +map de_dust2 \
+  +exec "eslgotv.cfg" \
   +exec "instance_${1}.cfg"
+
