ed2b58f2b5791b5b38b9946bc18a69f0c8da2a2a
Jacob Jenner Rasmussen <jacob.mindstorms@gmail.com>
2019-02-26 22:18:33 +0100

    work

--- roles/csgo/files/run-csgo.sh
+++ roles/csgo/files/run-csgo.sh
@@ -11,11 +11,9 @@ exec /home/steam/csgo/srcds_linux \
   +sv_setsteamaccount "$GSLT" \
   +tv_port "$GOTV_PORT" \
   +tv_password "$GOTV_PASSWORD" \
-  +tv_relaypassword "$GOTV_PASSWORD_ggwrelay" \
+  +tv_relaypassword "${GOTV_PASSWORD}_ggwrelay" \
   +sv_password "$PASSWORD" \
   +rcon_password "$RCON_PASSWORD" \
   +mapgroup mg_active \
   +map de_dust2 \
-  +exec "eslgotv.cfg" \
-  +exec "instance_${1}.cfg"
-
+  +exec "instance_${1}.cfg" \
